1c5e91682c7f42f4c4d0d92b4c438e407ffbc696
Crew Spence <csspence123@gmail.com>
2020-04-27 05:14:25 -0500

    Wrote working solution.

--- vowel_count_code_wars.js
+++ vowel_count_code_wars.js
@@ -6,10 +6,20 @@ We will consider a, e, i, o, and u as vowels for this Kata.
 The input string will only consist of lower case letters and/or spaces.
 */
 
-function getCount(str) {
-  var vowelsCount = 0;
-  
-  // enter your majic here
+const getCount = (str) => {
+  let vowelsCount = 0;
+  let vowels = {
+    a : 'a',
+    e : 'e',
+    i : 'i',
+    o : 'o',
+    u : 'u'
+  }
+  for(let i = 0; i < str.length; i++) {
+    if(vowels[str[i]] === str[i]) {
+      vowelsCount++;
+    }
+  }
   
   return vowelsCount;
 }
